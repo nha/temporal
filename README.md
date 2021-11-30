2dc01ba1ed2d2bbc7ade5883c9acc3ab7a4af4ee
nha <nha@users.noreply.github.com>
2021-11-30 15:14:07 +0000

    README complete sample

--- README.md
+++ README.md
@@ -11,11 +11,13 @@ This is a library to help using the [temporal java SDK](https://github.com/tempo
 ## Usage
 
 Don't. At least not yet.
-If you must, here is the greetings sample translated:
+If you must, here is the [greetings sample](https://github.com/temporalio/samples-java/blob/main/src/main/java/io/temporal/samples/hello/HelloActivity.java) translated:
 
 ```clojure
 
-(:require [nha.temporal :as t])
+(ns your.ns
+  (:require [nha.temporal :as t])
+  (:import [java.time Duration]))
 
 (def task-queue "HelloActivityTaskQueue")
 (def workflow-id "HelloActivityWorkflow")
@@ -23,39 +25,34 @@ If you must, here is the greetings sample translated:
 (t/def-workflow-interface
   GreetingWorkflow
   (^String getGreeting [param-not-this] "say something yo"))
-  
+
 (t/def-activity-interface
   GreetingActivities
   (composeGreeting [^String greeting ^String n]))
 
-(def reified-workflow
+(def my-workflow
   (reify GreetingWorkflow
     (getGreeting [this s]
-      (let [a (t/activity-stub GreetingActivities)]
-        (.composeGreeting a "HELLO " s)))))
-        
-(let [{:keys [test-env service client factory worker] :as component}
-      (sut/component task-queue
-                         [(class reified-workflow)]
-                         [my-activity])]
-        (let [^GreetingWorkflow workflow (sut/network-stub client
-                                                       GreetingWorkflow
-                                                       (sut/workflow-options task-queue workflow-id))]
+      (let [actv (t/activity-stub GreetingActivities
+                                  (t/activity-opts {:start-to-close-timeout (Duration/ofSeconds 2)}))]
+        (.composeGreeting actv "HELLO " s)))))
 
+(def my-activity (reify GreetingActivities
+                   (composeGreeting [this greetings n]
+                     (str greetings n))))
 
-(println (.getGreeting workflow "WORLD")) ;; will print "HELLO WORLD"
-      )                 
-                         
-       (sut/stop-component component))
-        
-...
+(let [{:keys [client] :as component} (t/component task-queue
+                                                  [(class my-workflow)]
+                                                  [my-activity])]
+  (let [^GreetingWorkflow workflow (t/network-stub client
+                                                   GreetingWorkflow
+                                                   (t/workflow-options task-queue workflow-id))]
+
+    (println (.getGreeting workflow "WORLD")) ;; <= will print "HELLO WORLD"
+    )
+  (t/stop-component component))
+
 ```
-
-
-## Goals
-
-
-
 
 ## Development
 
--- test/nha/readme_test.clj
+++ test/nha/readme_test.clj
@@ -1,118 +1,39 @@
 (ns nha.readme-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [nha.temporal :as t])
+  (:require [nha.temporal :as t])
   (:import [java.time Duration]))
 
-;; this talks to a real service that has to be installed locally
-;; so comment for now
-;; NOTE: there can be only one instance of interface/activity interface etc.?
-#_(ns nha.temporal-test
-  (:require
-   [clojure.test :refer [deftest testing is are]]
-   [nha.temporal :as sut]
-   [nha.temporal.testing :as testsut])
-  (:import
-   [io.temporal.activity Activity ActivityInterface ActivityMethod ActivityOptions]
-   [io.temporal.client WorkflowClient WorkflowOptions WorkflowClientOptions]
-   [io.temporal.common RetryOptions]
-   [io.temporal.failure ApplicationFailure]
-   [io.temporal.serviceclient WorkflowServiceStubs WorkflowServiceStubsOptions]
-   [io.temporal.testing TestEnvironmentOptions TestWorkflowEnvironment]
-   [io.temporal.worker Worker WorkerFactory WorkerOptions WorkerFactoryOptions]
-   [io.temporal.workflow Workflow WorkflowInterface WorkflowMethod]
-   [java.lang.annotation Retention RetentionPolicy Target ElementType]
-   [java.time Duration]
-   [java.util.concurrent TimeUnit]))
-
-;; same as the simple example. But uses the clj helpers defined
-
-;; hardcode the
-;; https://github.com/temporalio/samples-java/blob/main/src/main/java/io/temporal/samples/hello/HelloActivity.java
-;; to demonstrate that use from Clojure without this library is possible
-;; Left all the exploration steps in there too
+;; from the README.
+;; usage: uncomment, REPL run
 ;;
-;; https://github.com/temporalio/samples-java/blob/d92d48ca4431d6ea6af564a2d303fad9a9d3521b/src/main/java/io/temporal/samples/hello/HelloActivity.java
-;;
+;; (def task-queue "HelloActivityTaskQueue")
+;; (def workflow-id "HelloActivityWorkflow")
 
-(def task-queue "HelloActivityTaskQueue")
-(def workflow-id "HelloActivityWorkflow")
+;; (t/def-workflow-interface
+;;   GreetingWorkflow
+;;   (^String getGreeting [param-not-this] "say something yo"))
 
-(t/def-workflow-interface
-  GreetingWorkflow
-  (^String getGreeting [param-not-this] "say something yo"))
+;; (t/def-activity-interface
+;;   GreetingActivities
+;;   (composeGreeting [^String greeting ^String n]))
 
+;; (def my-workflow
+;;   (reify GreetingWorkflow
+;;     (getGreeting [this s]
+;;       (let [actv (t/activity-stub GreetingActivities
+;;                                   (t/activity-opts {:start-to-close-timeout (Duration/ofSeconds 2)}))]
+;;         (.composeGreeting actv "HELLO " s)))))
 
-(t/def-activity-interface
-  GreetingActivities
-  (composeGreeting [^String greeting ^String n]))
+;; (def my-activity (reify GreetingActivities
+;;                    (composeGreeting [this greetings n]
+;;                      (str greetings n))))
 
-(def reified-workflow
-  (reify GreetingWorkflow
-    (getGreeting [this s]
-      (try
-        (let [actv
-              (t/activity-stub
-               GreetingActivities
-               (t/activity-opts {:start-to-close-timeout (Duration/ofSeconds 2)
-                                   :retry-options          (t/retry-options {:backoff-coefficient 2
-                                                                               :maximum-attempts    2
-                                                                               :do-not-retry-coll   ["noretry"]})}))]
-          (println "GOT ACTIVITY ") ;; NOTE: cannot print actv
-          (.composeGreeting actv "HELLO " s))
-        (catch Exception ex
-          (println "ERROR - " ex)
+;; (let [{:keys [client] :as component} (t/component task-queue
+;;                                                   [(class my-workflow)]
+;;                                                   [my-activity])]
+;;   (let [^GreetingWorkflow workflow (t/network-stub client
+;;                                                    GreetingWorkflow
+;;                                                    (t/workflow-options task-queue workflow-id))]
 
-          ;; could also do this
-          ;; (throw (Activity/wrap ex))
-
-          "KO")))))
-
-
-;; https://github.com/temporalio/samples-java/blob/d92d48ca4431d6ea6af564a2d303fad9a9d3521b/src/main/java/io/temporal/samples/hello/HelloActivity.java#L109-L114
-
-
-(def my-activity (reify GreetingActivities
-                   (composeGreeting [this greetings n]
-                     (str greetings n))))
-
-(def test-worker-opts (t/worker-opts
-                       {:max-concurrent-activity-execution-size       2
-                        :max-concurrent-workflow-task-execution-size  2
-                        :max-concurrent-local-activity-execution-size 2
-                        :activity-poll-thread-count                   2
-                        :default-deadlock-detection-timeout           1000
-                        ;; :local-activity-worker-only? false
-                        :max-task-queue-activities-per-second         2
-                        :max-worker-activities-per-second             2
-                        }))
-
-;; https://www.javadoc.io/static/io.temporal/temporal-sdk/1.5.0/io/temporal/worker/WorkerFactoryOptions.Builder.html
-(def test-factory-opts (t/worker-factory-opts
-                        {:enable-logging-in-replay                                 false
-                         :max-workflow-thread-count                                2
-                         :workflow-cache-size                                      2
-                         :workflow-host-local-poll-thread-count                    2
-                         :workflow-host-local-task-queue-schedule-to-start-timeout (Duration/ofSeconds 2)
-                         }))
-
-(deftest run-workflow-readme
-  
-
-  (let [{:keys [test-env service client factory worker] :as component}
-        
-        (t/component task-queue
-                     [;; either of these work
-                      ;; GreetingWorkflowImplStep4
-                      (class reified-workflow)]
-                     [my-activity])]
-
-    (println "COMPONENT STARTED ")
-
-    (let [^GreetingWorkflow workflow (t/network-stub client
-                                                       GreetingWorkflow
-                                                       (t/workflow-options task-queue workflow-id))]
-
-      (is (= "HELLO WORLD" (.getGreeting workflow "WORLD")))
-      (println "TEST DONE"))
-
-    (t/stop-component component)))
+;;     (println (.getGreeting workflow "WORLD")) ;; <= will print "HELLO WORLD"
+;;     )
+;;   (t/stop-component component))
