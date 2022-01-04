b76d641994f5ff0cd731c364f7cdaa11810a952d
nha <nha@users.noreply.github.com>
2022-01-04 12:45:49 +0000

    cleanup build

--- build.clj
+++ build.clj
@@ -5,17 +5,11 @@
 (def lib 'org.clojars.nha/temporal)
 ;; temporal SDK version first
 (def version (format "1.5.0.%s" (b/git-count-revs nil)))
-;; (def class-dir "target/classes")
-;; (def basis (b/create-basis {:project "deps.edn"}))
-;;(def jar-file (format "target/%s-%s.jar" (name lib) version))
 
 (defn bb-opts [opts]
   (assoc opts
          :lib lib
-         :version version
-         ;; :class-dir class-dir
-         ;; :jar-file jar-file
-         ))
+         :version version))
 
 (defn eastwood "Run Eastwood." [opts]
   (-> opts (bb/run-task [:eastwood])))
@@ -24,7 +18,7 @@
   (-> opts
       (assoc :lib lib :version version)
       ;; (bb/run-task [:eastwood])
-      ;;(bb/run-tests)
+      (bb/run-tests)
       (bb/clean)
       (bb/jar)))
 
@@ -36,20 +30,6 @@
   (-> opts
       (assoc :lib lib :version version)
       (bb/install)))
-
-#_(defn jar [opts]
-  (bb/clean (bb-opts opts))
-  ;; (b/write-pom {:class-dir class-dir
-  ;;               :lib       lib
-  ;;               :version   version
-  ;;               :basis     basis
-  ;;               :src-dirs  ["src"]})
-  ;; (b/copy-dir {:src-dirs   ["src" "resources"]
-  ;;              :target-dir class-dir})
-  ;; (b/jar {:class-dir class-dir
-  ;;         :jar-file  jar-file})
-  )
-
 
 (defn deploy
   "Deploy the JAR to Clojars.
