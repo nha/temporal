50513acfcf441fec5e99ec8b69d9aeca16142bc6
nha <nha@users.noreply.github.com>
2022-01-04 12:58:32 +0000

    comment readme test

--- test/nha/readme_test.clj
+++ test/nha/readme_test.clj
@@ -5,33 +5,34 @@
 ;; from the README.
 ;; usage: uncomment, REPL run
 ;;
-(def task-queue "HelloActivityTaskQueue")
-(def workflow-id "HelloActivityWorkflow")
+(comment
+  (def task-queue "HelloActivityTaskQueue")
+  (def workflow-id "HelloActivityWorkflow")
 
-(t/def-workflow-interface
-  GreetingWorkflow
-  (^String getGreeting [param-not-this] "say something yo"))
+  (t/def-workflow-interface
+    GreetingWorkflow
+    (^String getGreeting [param-not-this] "say something yo"))
 
-(t/def-activity-interface
-  GreetingActivities
-  (composeGreeting [^String greeting ^String n]))
+  (t/def-activity-interface
+    GreetingActivities
+    (composeGreeting [^String greeting ^String n]))
 
-(defrecord MyWorkflowImpl []
-  GreetingWorkflow
-  (getGreeting [this s]
-    (let [actv (t/activity-stub
-                GreetingActivities
-                (t/activity-opts {:start-to-close-timeout (Duration/ofSeconds 2)}))]
-      (.composeGreeting actv "HELLO " s))))
+  (defrecord MyWorkflowImpl []
+    GreetingWorkflow
+    (getGreeting [this s]
+      (let [actv (t/activity-stub
+                  GreetingActivities
+                  (t/activity-opts {:start-to-close-timeout (Duration/ofSeconds 2)}))]
+        (.composeGreeting actv "HELLO " s))))
 
-(defrecord MyActivityImpl []
-  GreetingActivities
-  (composeGreeting [this greetings n]
-    (str greetings n)))
+  (defrecord MyActivityImpl []
+    GreetingActivities
+    (composeGreeting [this greetings n]
+      (str greetings n)))
 
-(let [{:keys [client] :as component} (-> (t/component task-queue)
-                                         (t/start-component [MyWorkflowImpl] [(MyActivityImpl.)]))]
-  (let [^GreetingWorkflow workflow (t/network-stub client GreetingWorkflow (t/workflow-options task-queue workflow-id))]
-    ;; this will print "HELLO WORLD"
-    (println (.getGreeting workflow "WORLD")))
-  (t/stop-component component))
+  (let [{:keys [client] :as component} (-> (t/component task-queue)
+                                           (t/start-component [MyWorkflowImpl] [(MyActivityImpl.)]))]
+    (let [^GreetingWorkflow workflow (t/network-stub client GreetingWorkflow (t/workflow-options task-queue workflow-id))]
+      ;; this will print "HELLO WORLD"
+      (println (.getGreeting workflow "WORLD")))
+    (t/stop-component component)))
